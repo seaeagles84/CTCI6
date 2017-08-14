22cf02bb7352939b08fc22185e0ce8949b23be36
seaeagles84 <30911270+seaeagles84@users.noreply.github.com>
2017-08-14 16:33:11 -0700

    Create p08_04.cpp
    See the output! (p08_04.out)
    BTW, the same problem as printing all the combinations of a given string e.g., "abcd". nC1 + nC2 + ... nCn

---
+++ p08_04.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printMyVec(vector<int> &rmyVec)
+{
+    cout << "{";
+    for (int i = 0; i < rmyVec.size(); i++)
+        cout << rmyVec[i] << ",";
+    cout << "}" << endl;
+}
+
+void powerSet(int *pArr, int begin, int end, vector<int> &rmyVec)
+{
+    cout << "begin:" << begin << "end:" << end << endl;
+    if (begin > end) {
+        cout << "Ulala!" << endl;
+        return;
+    } else if (begin == end) {
+        rmyVec.push_back(pArr[begin]);
+        printMyVec(rmyVec);
+        rmyVec.pop_back();
+        return;
+    }
+    
+    for (int i = begin; i <= end; i++)
+    {
+        rmyVec.push_back(pArr[i]);
+        printMyVec(rmyVec);
+        powerSet(pArr, i+1, end, rmyVec);
+        rmyVec.pop_back();
+    }
+    return;
+}
+
+int main ()
+{
+    int inputArr[] {3,4,5,6};
+    vector<int> myVec;
+    powerSet(inputArr, 0, sizeof(inputArr)/sizeof(int) - 1, myVec);
+    
+}
