7be161c5414d6ad4784a825a0a21fa523e89cd99
seaeagles84 <30911270+seaeagles84@users.noreply.github.com>
2017-09-20 15:17:02 -0700

    Update leetcode_311_sparse_matrix.cpp
    I guess the test set in leetcode as of today is not good enough.

--- leetcode_311_sparse_matrix.cpp
+++ leetcode_311_sparse_matrix.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
         vector<vector<int>> C(A.size(),vector<int>(B[0].size(),0));
         
+        //Seems to be this is not the ideal approach. Should prefer the previous one.
         for (int row = 0; row < A.size(); row++) {
             for (int k = 0; k < A[0].size(); k++) {
                 if (A[row][k]) {
