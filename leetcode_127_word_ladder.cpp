20020eee7eedc818865e59e7c4eeb10900526a51
seaeagles84 <30911270+seaeagles84@users.noreply.github.com>
2017-09-20 14:12:40 -0700

    Update leetcode_127_word_ladder.cpp
    Understood why we can use the same visited entry. 
    Also, understood how to just keep one count. Just segment the while loop into pieces.

--- leetcode_127_word_ladder.cpp
+++ leetcode_127_word_ladder.cpp
@@ -1,54 +1,42 @@
-struct Mystruct {
-    int cnt;
-    string str;
-};
-
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        queue<Mystruct> q_words;
+        queue<string> q_words;
         int n = beginWord.size();
         int len = wordList.size();
         int diff = 0;
-        int min_change = wordList.size() + 1; 
         vector<bool> visited(len, false);
         
-        Mystruct entry, temp;
-        entry.cnt = 0;
-        entry.str = beginWord;
+        int count = 0;
+        int change = 0;
+        string str;
         
-        string tempstr;
-        int tempcnt;
-        
-        q_words.push(entry);
+        q_words.push(beginWord);
         while (!q_words.empty()) {
-            temp = q_words.front();
-            q_words.pop();
+            change++;
+            count = q_words.size();
+            for (int idx = 0; idx < count; idx++) {
+                str = q_words.front();
+                q_words.pop();
             
-            tempstr = temp.str;
-            tempcnt = temp.cnt;
-            
-            for (int i = 0; i < len; i++) {
-                if (visited[i])
-                    continue;
+                for (int i = 0; i < len; i++) {
+                    if (visited[i])
+                        continue;
                 
-                diff = 0;
-                for (int ch = 0; ch < n; ch++) {
-                    if (tempstr[ch] != wordList[i][ch])
-                        diff++;
-                } 
+                    diff = 0;
+                    for (int ch = 0; ch < n; ch++) {
+                        if (str[ch] != wordList[i][ch])
+                            diff++;
+                    } 
                 
-                if (diff == 1) {
-                    if (wordList[i] == endWord) {
-                        min_change = tempcnt + 1;
-                        return min_change + 1;
-                    } else {
-                        visited[i] = true;
-                        
-                        entry.cnt = tempcnt + 1;
-                        entry.str = wordList[i];
-                        q_words.push(entry);
+                    if (diff == 1) {
+                        if (wordList[i] == endWord) {
+                            return change+1;
+                        } else {
+                            visited[i] = true;
+                            q_words.push(wordList[i]);
+                        }
                     }
                 }
             }
