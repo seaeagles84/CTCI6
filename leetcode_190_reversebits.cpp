af53a866605ee4fde97079b838a807f763f453a0
seaeagles84 <30911270+seaeagles84@users.noreply.github.com>
2017-09-21 11:35:54 -0700

    Create leetcode_190_reversebits.cpp

---
+++ leetcode_190_reversebits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t n_out = 0;
+        
+        if (n == 0)
+            return 0;
+        
+        int i;
+        int cnt = 0;
+        while (cnt < 32) {
+            i = n & 1;
+            n_out = n_out << 1 | i;
+            n = n >> 1;
+            cnt++;
+        }
+        
+        return n_out;
+        
+        /* Interesting solution from another submitter */
+        /*
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+        */
+    }
+};
